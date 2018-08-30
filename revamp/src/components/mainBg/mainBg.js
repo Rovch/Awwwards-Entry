2f5c4c82fc59dea5d7c479eef302e0351ee0f7dd
Rovch <wesleyjacksonjr33@hotmail.com>
2018-08-30 00:46:02 -0500

    Added  structure

---
+++ revamp/src/components/mainBg/mainBg.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./assets/mainBg.css";
+export default class MainBg extends Component {
+  render() {
+    return (
+      <div>
+        <div id="mainBg"></div>
+
+      </div>
+    )
+  }
+}
---
+++ revamp/src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./assets/home.css";
+import MainBg from "../../components/mainBg";
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <MainBg />
+
+      </div>
+    )
+  }
+}
